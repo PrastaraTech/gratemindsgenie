3e548f3b4d31091b32e715022fb37a59878fc8dd
Prastara Technologies <98683199+PrastaraTech@users.noreply.github.com>
2023-10-09 07:34:23 +0000

    google

--- Hello.py
+++ Hello.py
@@ -14,8 +14,23 @@
 import google.generativeai as palm
 import streamlit as st
 from streamlit_chat import message
-
+from bs4 import BeautifulSoup
+import requests
 import re
+
+# secure = 'bwhzG2utun5t_QKbsEYF9SN37sGeuL-iLH66PyXpmuK_QMRlIvFILUhKacMk1gPrTzraUg.'
+
+form_data={'Email': 'gmgenie9@gmail.com', 'Passwd': '!h7^m#6*a@e'}
+post = "https://accounts.google.com/signin/challenge/sl/password"
+
+with requests.Session() as s:
+    soup = BeautifulSoup(s.get("https://mail.google.com").text)
+    for inp in soup.select("#gaia_loginform input[name]"):
+        if inp["name"] not in form_data:
+            form_data[inp["name"]] = inp["value"]
+    s.post(post, form_data)
+    html = s.get("https://bard.google.com").content
+
 
 st.set_page_config(
         page_title="GrateMinds - Genie",
